bf25893d271e45fa024fe2383817b470b641ec5e
Ramanathan Vishnampet <ramanathan.vishnampet@utcsdevtst.onmicrosoft.com>
2020-09-14 02:51:08 +0000

    Added remote VSCode settings.json.

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ dotfiles_dir=~/dotfiles
 rm -rf ~/.bashrc > /dev/null 2>&1
 ln -sf $dotfiles_dir/bashrc ~/.bashrc 
 
+rm -rf ~/.vscode-remote/data/Machine/settings.json
+ln -sf $dotfiles_dir/settings.json ~/.vscode-remote/data/Machine/settings.json
+
 command -v poetry > /dev/null 2>&1 || curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
